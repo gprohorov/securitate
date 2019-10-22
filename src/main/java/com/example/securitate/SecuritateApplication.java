83d13488c65e9df1e2dbe96d6d555eed5db5d283
george <yuramail222@gmail.com>
2019-10-22 17:11:28 +0300

    Initial commit

---
+++ src/main/java/com/example/securitate/SecuritateApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securitate/SecuritateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
